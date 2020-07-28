16a94a2e0c160fb30e159196dc9f77120f9972cc
eidorien <68266570+eidorien@users.noreply.github.com>
2020-07-27 21:06:27 -0700

    show cards

--- cardmagic.js
+++ cardmagic.js
@@ -11,7 +11,22 @@ function magicCard(name, redCost, blueCost, greenCost, whiteCost, blackCost) {
     }
 }
 
-var silence = new magicCard('silence', 0, 0, 0, 2, 0);
-var brainstorm = new magicCard('brainstorm', 0, 1, 0, 0, 0);
+function cardInfo(card, id) {
+    var el = document.getElementById(id);
+    el.getElementsByClassName('cardName')[0].textContent = card.name;
+    el.getElementsByClassName('redCost')[0].textContent = card.redCost;
+    el.getElementsByClassName('blueCost')[0].textContent = card.blueCost;
+    el.getElementsByClassName('greenCost')[0].textContent = card.greenCost;
+    el.getElementsByClassName('whiteCost')[0].textContent = card.whiteCost;
+    el.getElementsByClassName('blackCost')[0].textContent = card.blackCost;
+    el.getElementsByClassName('totalCost')[0].textContent = 'Total cost: ' + card.totalCost();
+}
+
+var silence = new magicCard('Silence', 0, 0, 0, 1, 1);
+var brainstorm = new magicCard('Brainstorm', 0, 2, 0, 0, 0);
+
+cardInfo(silence, 'cardNum1');
+cardInfo(brainstorm, 'cardNum2');
 
 console.log('the cost of ' + silence.name + ' is equal to ' + silence.totalCost() + ' mana');
+console.log('the cost of ' + brainstorm.name + ' is equal to ' + brainstorm.totalCost() + ' mana');
